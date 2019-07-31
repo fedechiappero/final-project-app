c35bb7112f79813b9a4e531f5c2235a5b74e4544
Federico Bit <fedechiappero06@gmail.com>
2019-07-31 11:48:48 -0300

    Add status for request

---
+++ src/AppBundle/Entity/EstadoPedido.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EstadoPedido
+ *
+ * @ORM\Table(name="estado_pedido")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EstadoPedidoRepository")
+ */
+class EstadoPedido
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \AppBundle\Entity\Pedido
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Pedido")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_pedido", referencedColumnName="id")
+     * })
+     */
+    private $idPedido;
+
+    /**
+     * @var \AppBundle\Entity\Estado
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Estado")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_estado", referencedColumnName="id")
+     * })
+     */
+    private $idEstado;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha", type="datetime")
+     */
+    private $fecha;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idPedido
+     *
+     * @param integer $idPedido
+     *
+     * @return EstadoPedido
+     */
+    public function setIdPedido($idPedido)
+    {
+        $this->idPedido = $idPedido;
+
+        return $this;
+    }
+
+    /**
+     * Get idPedido
+     *
+     * @return int
+     */
+    public function getIdPedido()
+    {
+        return $this->idPedido;
+    }
+
+    /**
+     * Set idEstado
+     *
+     * @param integer $idEstado
+     *
+     * @return EstadoPedido
+     */
+    public function setIdEstado($idEstado)
+    {
+        $this->idEstado = $idEstado;
+
+        return $this;
+    }
+
+    /**
+     * Get idEstado
+     *
+     * @return int
+     */
+    public function getIdEstado()
+    {
+        return $this->idEstado;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     *
+     * @return EstadoPedido
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+}
+
