2ce1132dc5fad5f5cb20d73d48b8eb0259994042
Federico Bit <fedechiappero06@gmail.com>
2019-08-29 22:05:31 -0300

    Add landing page and basic nav
    contact form doesn't work so far

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,45 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('default/contact.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/sendemail", name="sendemail")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function sendEmailAction(Request $request)
+    {
+        $name = $request->request->get('name');
+        $email = $request->request->get('email');
+        $subject = $request->request->get('subject');
+        $message = $request->request->get('message');
+        $mail = \Swift_Message::newInstance()
+            ->setSubject("No responder")
+            ->setFrom($email)
+            ->setTo('fedechiappero06@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    'Email/consulta.html.twig',
+                    array('name' => $name,
+                        'email' => $email,
+                        'subject' => $subject,
+                        'message'=>$message)
+                ),
+                'text/html'
+            );
+
+        $this->get('mailer')->send($mail);
+
+        return $this->redirect($this->generateUrl('homepage'));
+    }
 }
